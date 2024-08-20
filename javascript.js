fd0ae5c81b3279a8381d4e2eaac0f591e3547da4
plastic <promotionplastic13@gmail.com>
2024-08-20 23:51:57 +0530

    script for new buttons

--- javascript.js
+++ javascript.js
@@ -15,32 +15,49 @@
 let slider = document.querySelector("#myRange");
 let output = document.querySelector("#demo");
 
-output.innerHTML = slider.value; // Display the default slider value
 let number_of_pixels = 55*55
-let height_of_pixel = (400)/slider.value
-let width_of_pixel = (400)/slider.value
+let height_of_pixel = (600)/slider.value
+let width_of_pixel = (600)/slider.value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
-  console.log(slider.value)
   number_of_pixels = slider.value * slider.value
-  height_of_pixel = (400/slider.value)
-  width_of_pixel = (400/slider.value)
+  height_of_pixel = (600/slider.value)
+  width_of_pixel = (600/slider.value)
   console.log('height'+height_of_pixel)
   console.log('width'+width_of_pixel)
   console.log(number_of_pixels)
   createGrid()
 }
-console.log(number_of_pixels+'oog')
+
 
 
 let container = document.querySelector('.gab')
 
+let color
+let isDrawing = false
+let rainbowMode = false
+let colorMode = true
 
+function randomcolor(){
+color = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`}
 
-
+document.querySelector('.colormode').addEventListener('click',()=>{colorMode = true;
+  rainbowMode = false;
+  color = document.querySelector('#colorpicker').value})
+document.querySelector('.rainbow').addEventListener('click',()=>{colorMode = false;
+  rainbowMode = true;
+  randomcolor()
+})
 
 function createGrid(){
+if (colorMode){
+  let colorpicker = document.querySelector('#colorpicker')
+  color = colorpicker.value
+  colorpicker.oninput = function(){
+  color = this.value
+}}
+
+
 container.innerHTML = '';
 let grid
 for(i=0;i<number_of_pixels;i++){
@@ -53,7 +70,18 @@ let pixel = document.querySelectorAll('.pixel')
 for(let n=0;n<number_of_pixels;n++){
     pixel[n].style.height = height_of_pixel + 'px'
     pixel[n].style.width = width_of_pixel + 'px'
-    pixel[n].addEventListener('mouseover',function(){ (pixel[n].style.backgroundColor = 'white')})
+    pixel[n].addEventListener('mousedown',function(e){ 
+      e.preventDefault()
+      isDrawing = true;
+      (pixel[n].style.backgroundColor = color)})
+    pixel[n].addEventListener('mouseup',function(){ 
+      isDrawing = false})
+    pixel[n].addEventListener('mousemove',function(){ 
+      if (isDrawing){
+      if(rainbowMode){
+        randomcolor()
+      }
+      (pixel[n].style.backgroundColor = color)}})
 }
 
 
@@ -62,5 +90,7 @@ for(let n=0;n<number_of_pixels;n++){
 
 createGrid()
 
+let clearButton = document.querySelector('.clearButton')
+clearButton.addEventListener('click',()=>{createGrid()})
 
 
